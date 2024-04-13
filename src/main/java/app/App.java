71b07bb7cc53937586dd571d9c63f3c9932e11af
OlyoshaOlyosha <remaxux@proton.me>
2024-04-13 16:09:10 +0300

    Made a Card object

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,4 +1,19 @@
 package app;
+import bank.Card;
 
 public class App {
+    public static void main(String[] args) {
+        testCode();
+    }
+
+    // Метод для тестирования программы
+    public static void testCode(){
+        Card myCard = new Card();
+        System.out.println("Карта: " + myCard.getCardNumber());
+        System.out.println("Пинкод: " + myCard.getPinCode());
+
+        // Изменяем пинкод
+        myCard.setPinCode("0001");
+        System.out.println("Новый пинкод: " + myCard.getPinCode());
+    }
 }
